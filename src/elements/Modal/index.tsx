c82adcc522e3a0b4d8e6506fd9386d93b212f7f5
lusivelin <linhtetpaing@geniefintech.com>
2023-02-21 13:39:20 +0630

    fix: modal props improvement

--- src/elements/Modal/index.tsx
+++ src/elements/Modal/index.tsx
@@ -1,57 +1,69 @@
+import { FC, PropsWithChildren, ReactNode } from "react";
+import Portal from "../Portal";
 import {
-  FC,
-  PropsWithChildren,
-  ReactNode,
-} from "react";
-import Portal from "../../component/Portal";
-import {
-  crossIcon,
-  modalContainer,
-  modalContent,
-  modalContentHeader,
-  modalMask,
-  modalContentBody,
+	crossIcon,
+	modalContainer,
+	modalContent,
+	modalContentHeader,
+	modalMask,
+	modalContentBody,
 } from "./style";
 import { CrossIcon } from "../Icon";
+import { CSS } from "@stitches/core";
 
 export type ModalProps = {
-  open: boolean;
-  onClose: (value: boolean) => void;
+	open: boolean;
+	onClose: (value: boolean) => void;
+	uiProps?: {
+		container?: CSS;
+		content?: CSS;
+		mask?: CSS;
+	};
 };
 
 const Modal: FC<PropsWithChildren<ModalProps>> = ({
-  children,
-  open,
-  onClose,
+	children,
+	open,
+	onClose,
+	uiProps,
 }) => {
-  const result: ReactNode = (
-    <>
-      <header className={modalContentHeader()}>
-        <h3>Hello This is title</h3>
-        <CrossIcon onClick={() => onClose(false)} className={crossIcon()} />
-      </header>
-      <main className={modalContentBody()}>
-        <div>Description bla bla bla</div>
-      </main>
-    </>
-  );
-  return (
-    <div>
-      <button
-        onClick={() => {
-          onClose(!open);
-        }}
-      >
-        switch
-      </button>
-      <Portal>
-        <div className={modalContainer({ open })}>
-          <div className={modalContent({ open })}>{children || result}</div>
-          <div onClick={() => onClose(false)} className={modalMask()}></div>
-        </div>
-      </Portal>
-    </div>
-  );
+	const { container, content, mask } = uiProps || {};
+	const result: ReactNode = (
+		<>
+			<header className={modalContentHeader()}>
+				<h3>Hello This is title</h3>
+				<CrossIcon
+					onClick={() => onClose(false)}
+					className={crossIcon()}
+				/>
+			</header>
+			<main className={modalContentBody()}>
+				<div>Description Here</div>
+			</main>
+		</>
+	);
+	return (
+		<div>
+			<button
+				onClick={() => {
+					onClose(!open);
+				}}
+			>
+				switch
+			</button>
+			<Portal>
+				<div className={modalContainer({ open, css: container })}>
+					<div className={modalContent({ css: content })}>
+						{children || result}
+					</div>
+					<div
+						onClick={() => onClose(false)}
+						className={modalMask({ css: mask })}
+					></div>
+				</div>
+			</Portal>
+		</div>
+	);
 };
 
 export default Modal;
--- src/elements/Modal/style.ts
+++ src/elements/Modal/style.ts
@@ -1,73 +1,72 @@
 import { css } from "../../stitches.config";
 
 export const modalContent = css({
-  background: "#fff",
-  padding: 20,
-  zIndex: 10,
-  boxShadow:
-    "0 6px 16px 0 rgb(0 0 0 / 8%), 0 3px 6px -4px rgb(0 0 0 / 12%), 0 9px 28px 8px rgb(0 0 0 / 5%)",
-  borderRadius: 15,
-  top: 0,
+	background: "#fff",
+	padding: 20,
+	zIndex: 9,
+	boxShadow:
+		"0 6px 16px 0 rgb(0 0 0 / 6%), 0 3px 6px -4px rgb(0 0 0 / 12%), 0 9px 28px 8px rgb(0 0 0 / 5%)",
+	borderRadius: 15,
+	top: 0,
 });
 
 export const modalContentHeader = css({
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
+	display: "flex",
+	justifyContent: "space-between",
+	alignItems: "center",
 });
 
 export const modalContentBody = css({
-  width: 480,
-  height: 480,
-  overflowY: "auto",
+	width: 480,
+	height: 480,
+	overflowY: "auto",
 });
 
 export const modalContainer = css({
-  width: "100%",
-  height: "100%",
+	width: "100%",
+	height: "100%",
 
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
 
-  perspective: 1000,
-  position: "fixed",
-  top: 0,
-  transitionProperty: "opacity, transform",
+	position: "fixed",
+	top: 0,
+	zIndex: 10,
 
-  variants: {
-    open: {
-      true: {
-        transitionProperty: "visibility, opacity",
-        transitionDuration: "0s, 0.5s",
-      },
-      false: {
-        opacity: 0,
-        visibility: "hidden",
-        transitionDelay: "0s, 0.5s",
-        transitionDuration: "0.5s, 0s",
-        transitionProperty: "opacity, visibility",
-      },
-    },
-  },
+	variants: {
+		open: {
+			true: {
+				transitionProperty: "visibility, opacity",
+				transitionDuration: "0s, 0.5s",
+			},
+			false: {
+				opacity: 0,
+				visibility: "hidden",
+				transitionDelay: "0s, 0.5s",
+				transitionDuration: "0.5s, 0s",
+				transitionProperty: "opacity, visibility",
+			},
+		},
+	},
 
-  defaultVariants: {
-    open: false,
-  },
+	defaultVariants: {
+		open: false,
+	},
 });
 
 export const modalMask = css({
-  width: "100%",
-  height: "100vh",
-  background: "#000",
-  opacity: 0.45,
+	width: "100%",
+	height: "100%",
+	background: "#000",
+	opacity: 0.45,
 
-  position: "absolute",
-  top: 0,
+	position: "fixed",
+	top: 0,
 });
 
 export const crossIcon = css({
-  width: "2.5em",
-  height: "2.5em",
-  cursor: "pointer",
+	width: "2.5em",
+	height: "2.5em",
+	cursor: "pointer",
 });
